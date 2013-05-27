3ea85c3f1edeb76205d7304003557a7a93be1b83
Meris Williams <merisw@gmail.com>
2013-05-27 10:33:11 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HuskyTracker::Application.config.secret_token = '1177d52e796fa20e12466d2545b9600aba02ac215ff50cc0b6ca0fb6dc83e0a71309c8c6ef8fac2bb1194822e759f7cfb20498bafe096a757492095e5b452d60'
